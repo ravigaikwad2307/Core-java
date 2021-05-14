6b567917d1206a55c0f1560a15df292612aaf29c
ravigaikwad2307 <83277924+ravigaikwad2307@users.noreply.github.com>
2021-05-14 09:00:25 +0530

    Exception handling

---
+++ Excep1.java
@@ -0,0 +1,14 @@
+public class Excep1 {
+    public static void main(String[] args){
+        try{
+            //int a = 10/0;
+            //System.out.println("Ram".charAt(3));
+            int a[] = new int[-1];
+        }
+        catch(ArithmeticException | StringIndexOutOfBoundsException | NegativeArraySizeException e){
+            System.out.println("-----------");
+            e.printStackTrace();
+
+        }
+    }
+}
