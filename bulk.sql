4b01f91a8076daa7a753e6bc7e469f648817b634
ravigaikwad2307 <83277924+ravigaikwad2307@users.noreply.github.com>
2026-01-19 22:17:02 +0530

    Update bulk.sql

--- bulk.sql
+++ bulk.sql
@@ -1,3 +1,4 @@
+--------
 DECLARE
     TYPE emp_tab is table of employees%rowtype;
     l_emp emp_tab;
@@ -7,7 +8,7 @@ begin
     dbms_output.PUT_LINE(l_emp(i).employee_id || ' ' || l_emp(i).first_name);
     end loop;
 end;
-
+-----
 DECLARE
     type emp_id_tab is table of employees.EMPLOYEE_ID%TYPE;
     emp_id emp_id_tab := emp_id_tab(41,42,43);
@@ -17,3 +18,21 @@ BEGIN
         set salary = salary + 10000
         where employee_id = emp_id(i);
 END;
+
+-----
+
+declare
+    type emp_tab IS TABLE of employees%ROWTYPE;
+    l_emp emp_tab;
+
+BEGIN
+    select * bulk collect into l_emp
+    from EMPLOYEES where manager_id = 25;
+
+    forall i in l_emp.first .. l_emp.last 
+        update employees 
+        set salary = salary * 1.1
+        where employee_id = l_emp(i).employee_id;
+END;
+
+select * from employees where MANAGER_ID =25;
